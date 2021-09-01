c3224d9eefbd4b93acd8b93c2ddf3ea8b429deed
vinicius <vinicius.tecbcc@gmail.com>
2021-09-01 11:26:54 -0300

    usando pipes para formatar data e valor

--- src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -8,12 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class LancamentosPesquisaComponent {
 
   lancamentos : any[] = [
-    {tipo: 'DESPESA', descricao: 'Compra de Pao', dataVencimento: '30/06/2017', dataPagamento: null, valor: 4.55, pessoa: 'Padaria do José'},
-    {tipo: 'RECEITA', descricao: 'Salario', dataVencimento: '30/06/2017', dataPagamento: null, valor: 1094.00, pessoa: 'Joao Pedro'},
-    {tipo: 'DESPESA', descricao: 'Roupas', dataVencimento: '30/06/2017', dataPagamento: null, valor: 104.21, pessoa: 'Wesley silva'},
-    {tipo: 'DESPESA', descricao: 'Fifa 22', dataVencimento: '30/06/2017', dataPagamento: null, valor: 304.12, pessoa: 'Sergio Nascimento'},
-    {tipo: 'DESPESA', descricao: 'Controle PS4', dataVencimento: '30/06/2017', dataPagamento: null, valor: 234.60, pessoa: 'Daniel Alves'},
-    {tipo: 'DESPESA', descricao: 'Mercadinho', dataVencimento: '30/06/2017', dataPagamento: null, valor: 14.51, pessoa: 'Messi'}
+    {tipo: 'DESPESA', descricao: 'Compra de Pao', dataVencimento: new Date(2017,6,30), dataPagamento: null, valor: 4.55, pessoa: 'Padaria do José'},
+    {tipo: 'RECEITA', descricao: 'Salario', dataVencimento: new Date(2017,6,30), dataPagamento: null, valor: 1094.00, pessoa: 'Joao Pedro'},
+    {tipo: 'DESPESA', descricao: 'Roupas', dataVencimento: new Date(2017,6,30), dataPagamento: null, valor: 104.21, pessoa: 'Wesley silva'},
+    {tipo: 'DESPESA', descricao: 'Fifa 22', dataVencimento: new Date(2017,6,30), dataPagamento: null, valor: 304.12, pessoa: 'Sergio Nascimento'},
+    {tipo: 'DESPESA', descricao: 'Controle PS4', dataVencimento: new Date(2017,6,30), dataPagamento: null, valor: 234.60, pessoa: 'Daniel Alves'},
+    {tipo: 'DESPESA', descricao: 'Mercadinho', dataVencimento: new Date(2017,6,30), dataPagamento: null, valor: 14.51, pessoa: 'Messi'}
   ]
 
 }
