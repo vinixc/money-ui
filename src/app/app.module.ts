f39ee89ce78b767ba7ff2d1b5749f5dde719ed0d
vinicius <vinicius.tecbcc@gmail.com>
2021-11-02 10:58:47 -0300

    adicionando roteamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { PessoaCadastroComponent } from './pessoas/pessoa-cadastro/pessoa-cadastro.component';
+import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
+import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { CoreModule } from './core/core.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { NgModule } from '@angular/core';
@@ -5,13 +8,24 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
 
+const routes : Routes = [
+
+  {path: 'lancamentos', component: LancamentosPesquisaComponent},
+  {path: 'lancamentos/novo', component: LancamentoCadastroComponent},
+  {path: 'pessoas', component: PessoasPesquisaComponent},
+  {path: 'pessoas/novo', component: PessoaCadastroComponent},
+
+]
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     LancamentosModule,
     PessoasModule,
     CoreModule,
