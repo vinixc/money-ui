275c2570ff71ee9fae520fd6cc80fa5b430ea94b
vinicius <vinicius.tecbcc@gmail.com>
2021-11-07 17:17:50 -0300

    tratando acesso de usuarios deslogados

--- src/app/seguranca/auth.guard.ts
+++ src/app/seguranca/auth.guard.ts
@@ -16,6 +16,21 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
+      if(this.auth.isAccessTokenInvalido()){
+
+        return this.auth.obterNovoAccessToken()
+          .then(() =>{
+            if(this.auth.isAccessTokenInvalido()){
+
+              this.router.navigate(['/login']);
+              return false;
+            }
+
+            return true;
+          })
+
+      }
+
       //Se nao tiver qualquer uma das permissoes nao deixa continuar.
       if(route.data.roles && !this.auth.temQualquerPermissao(route.data.roles)){
 
